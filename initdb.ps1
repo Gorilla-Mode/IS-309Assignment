ba35fb8752b71ccd7131a277e3e5b39b5f9aabc3
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-25 23:52:59 +0100

    build: initial script for deploy

---
+++ initdb.ps1
@@ -0,0 +1,158 @@
+param(
+    [Switch]$h, # helper
+    [Switch]$ni, # no inject
+    [Switch]$ne, # no execute
+    [Switch]$r, #Rebuilds database
+    [Switch]$rc, #restarts containers
+    [Switch]$l #attaches logger
+)
+
+if($h)
+{
+    Write-Host "WARNING: script will not catch any errors inside docker container. Check tables manually for now"
+    Write-Host "    -h: helper. Displays what you're reading rn"
+    Write-Host "    -ni: no inject. Prevents sql from being injected to container"
+    Write-Host "    -ne: no execute. Prevents sql script in container from being executed"
+    Write-Host "    -r: rebuild. Drops database, and reacreates it"
+    Write-Host "    -rc: restart containers. Restarts containers"
+    Write-Host "        -l: logger. Attaches logging to cli, allows confirmation of integration tests. requires -rc"
+    return
+}
+
+if($l -and !$rc)
+{
+    Write-Host "    ERROR: Illegal flag. Must run -rc to run -l"
+    return
+}
+
+if ($r)
+{
+    Write-Host "WARNING: Selected flag will drop database!"
+    $conf = Read-Host "     Confirm [Y]"
+
+    if ($conf -notlike "Y")
+    {
+        Write-Host "     Aborted"
+        exit
+    }
+}
+
+$scriptAbsolutePath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$sqlAbsolutePath = $scriptAbsolutePath+"/db.sql"
+$SqlUserPath = $scriptAbsolutePath+"/user.sql"
+$envAbsolutePath = $scriptAbsolutePath+"/.env"
+
+#tests
+$sqlFileExists = Test-Path -Path $sqlAbsolutePath
+$envFileExists = Test-Path -Path $envAbsolutePath
+
+if(!$sqlFileExists)
+{
+    Write-Host "Error: db.sql not found"
+    return
+}
+if(!$envFileExists)
+{
+    Write-Host "Error: .env file not found, make sure to make env file first"
+    return
+}
+
+#stores env file as hashtable, for ease of access
+$envHash = @{}
+
+if(!$ni)
+{
+    try
+    {
+        #injects sql script to container
+        Write-Host "Injecting sql from @ $sqlAbsolutePath to container..."
+        docker cp $sqlAbsolutePath db:/
+    }
+    catch
+    {
+        Write-Host "Sql injection failed"
+        return
+    }
+}
+
+#populates hashtable with variables and values from env
+Get-Content $envAbsolutePath | foreach {
+    $variable, $value = $_.split('=')
+    if ([string]::IsNullOrWhiteSpace($variable) -or $variable.Contains('#'))
+    {
+        #skips empty lines or comments
+        return
+    }
+    $envHash["$variable"] = "$value"
+}
+
+if($r) #goofy script maybe redo
+{
+    try
+    {
+        Write-Host "Rebuilding Database"
+        Write-Host "    Generating drop database sql script..."
+        $null = New-Item -Path $scriptAbsolutePath -Name "dropdb.sql" -Value "DROP DATABASE $($envHash['MYSQL_DATABASE']); CREATE DATABASE $($envHash['MYSQL_DATABASE']);" -Force
+        $dropsqlAbsolutePath = $scriptAbsolutePath+"/dropdb.sql"
+
+        Write-Host "    Injecting sql from @ $dropsqlAbsolutePath to container..."
+        docker cp -q $dropsqlAbsolutePath db:/
+        Write-Host "    Removing used script from @ $dropsqlAbsolutePath..."
+        Remove-Item -Path $dropsqlAbsolutePath
+        Write-Host "    Used script removed"
+
+        Write-Host "    Executing database sql script on $($envHash['MYSQL_DATABASE'])..."
+        docker exec db sh -c "mariadb $($envHash['MYSQL_DATABASE']) -u root -p$($envHash['MYSQL_ROOT_PASSWORD']) <dropdb.sql"
+        Write-Host "    Database dropped!"
+    }
+    catch
+    {
+        return
+    }
+}
+
+if(!$ne)
+{
+    try
+    {
+        #runs sql script on container
+        Write-Host "Executing database sql script on $($envHash['MYSQL_DATABASE'])..."
+        docker exec db sh -c "mariadb $($envHash['MYSQL_DATABASE']) -u root -p$($envHash['MYSQL_ROOT_PASSWORD']) <db.sql"
+        Write-Host "    Sql script executed, tables built"
+
+        Write-Host "    Generating sql user script..."
+        $null = New-Item -Path $scriptAbsolutePath -Name "user.sql" -Force
+
+        Add-Content -Path $SqlUserPath -Value "START TRANSACTION;"
+        Add-Content -Path $SqlUserPath -Value "REVOKE ALL PRIVILEGES ON * FROM '$($envHash['MYSQL_USER'])'@'%';"
+        Add-Content -Path $SqlUserPath -Value "GRANT SELECT, INSERT, UPDATE ON * TO '$($envHash['MYSQL_USER'])'@'%';"
+        Add-Content -Path $SqlUserPath -Value "REVOKE GRANT OPTION ON * FROM '$($envHash['MYSQL_USER'])'@'%';"
+        Add-Content -Path $SqlUserPath -Value "FLUSH PRIVILEGES;"
+        Add-Content -Path $SqlUserPath -Value "COMMIT;"
+
+        Write-Host "Injecting user sql from @ $SqlUserPath to container..."
+        docker cp $SqlUserPath db:/
+
+        Write-Host "    Executing sql user script on $($envHash['MYSQL_DATABASE'])..."
+        docker exec db sh -c "mariadb $($envHash['MYSQL_DATABASE']) -u root -p$($envHash['MYSQL_ROOT_PASSWORD']) <user.sql"
+
+        Remove-Item -Path $SqlUserPath
+        Write-Host "    Used script removed"
+    }
+    catch
+    {
+        return
+    }
+}
+if($rc) #Restart container with logger to confirm integration tests
+{
+    Write-Host "Restaring containers detatched.."
+    docker-compose stop
+    docker-compose start
+    Write-Host "Containers restared, logger attached"
+    if($l)
+    {
+        Write-Host "logger attached"
+        docker-compose logs -f --since 0m
+    }
+}
---
+++ setup.ps1
@@ -0,0 +1,122 @@
+param(
+    [Switch]$c, # runs a clean install
+    [Switch]$f, # overwrites existing env file
+    [Switch]$nc, # prevents compose from running
+    [Switch]$r, # prevents env from being generated
+    [Switch]$d, # Detached, so it doesn't hog the cli
+    [Switch]$h # help
+)
+
+if($h)
+{
+    Write-Host "    -c: clean. Cleans up previous install. Removes any containers, images and volumes defined in docker-compose"
+    Write-Host "    -f: force. Forces creation of .env file, overwriting if one already exists"
+    Write-Host "    -nc: no compose. Stops docker-compose up from running"
+    Write-Host "    -r: rebuild. Stops .env file from being generated, allowing rebuild"
+    Write-Host "    -d: detach. Composes detached, running service in background leaving cli clear for more shi"
+    Write-Host "    -h: helper. Displays what you're reading rn"
+    
+    return
+}
+
+if ($nc -and ($r -or $c -or $d))
+{
+    Write-Host "    ERROR: Wack flag combo, script TERMINATED"
+    return
+}
+
+if ($c)
+{
+    Write-Host "WARNING: Selected flag delete images and volumes defined in docker compose!"
+    $conf = Read-Host "     Confirm [Y]"
+    
+    
+    if ($conf -notlike "Y")
+    {
+        Write-Host "     Aborted"
+        exit
+    }
+}
+
+# retiveves the path of the this script
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+# --- env ----
+if (!$r)
+{
+    if($f)
+    {
+        # overwrites file if it exists
+        New-Item -Path $scriptDir -Name ".env" -ItemType "File" -Force
+    }
+    else
+    {
+        Write-Host $scriptDir
+        # checks if file already exists
+        if((Test-Path -Path ($scriptDir + "/.env")))
+        {
+            Write-Output "ERROR: .env file aready exists in directory, use -f to overwrite"
+            return
+        }
+        else
+        {
+            New-Item -Path $scriptDir -Name ".env" -ItemType "File"
+        }
+    }
+    
+    #Passowrd stored as secure string to hide input mostly
+    $secureDatabaseRootPwd = Read-Host "Create root password" -AsSecureString
+    $databaseName = Read-Host "Create database name"
+    $databaseUserName = Read-Host "Create user name"
+    $databaseUserPwd = Read-Host "Create user password" -AsSecureString
+    
+    #returns password to plain text
+    $DatabaseRootPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureDatabaseRootPwd))
+    $databaseUserPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($databaseUserPwd))
+
+    try
+    {
+        #populates env file
+        Write-Host "    Populating .env @ $scriptDir"
+
+        Add-Content -Path ($scriptDir + "/.env") -Value ("MYSQL_ROOT_PASSWORD=$databaseRootPwd")
+        Write-Host "        - SQL root password inserted"
+        Add-Content -Path ($scriptDir +"/.env") -Value ("POSTGRES_DB=$databaseName")
+        Write-Host "        - SQL database name inserted"
+        Add-Content -Path ($scriptDir +"/.env") -Value ("POSTGRES_USER=$databaseUserName")
+        Write-Host "        - SQL username inserted"
+        Add-Content -Path ($scriptDir +"/.env") -Value ("POSTGRES_PASSWORD=$databaseUserPwd")
+        Write-Host "        - SQL user password inserted"
+        Add-Content -Path ($scriptDir +"/.env") -Value ("EXTERNALCONNECTION=postgresql://$($databaseUserName):$($databaseUserPwd)@localhost:5432/$databaseName")
+        Write-Host "        - External connection string inserted"
+
+        Write-Host "    .env populated successfully"
+    }
+    catch
+    {
+        return
+    }
+}
+
+if ($nc)
+{
+    return
+}
+
+# ---- Docker ----
+
+cd $scriptDir
+
+if ($c)
+{
+    #removes any previous containers, images and volumes defined in docker-compose.yml
+    #this is done so that new password applies to database.
+    docker-compose down -v --rmi "all"
+}
+if($d)
+{
+    docker-compose up -d
+}
+else
+{
+    docker-compose up
+}
